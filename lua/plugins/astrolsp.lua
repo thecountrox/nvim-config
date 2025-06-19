fe3ab70748d846611f4ffaa77fac88fe4430461a
Prajol David <prajoldavid2@gmail.com>
2025-06-19 11:13:06 +0530

    Change python lsp settings (now has auto imports)

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -45,6 +43,26 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              pycodestyle = { enabled = false },
+              pyflakes = { enabled = true },
+              mccabe = { enabled = true },
+              yapf = { enabled = false },
+              autopep8 = { enabled = true },
+              pylint = { enabled = false },
+              -- disable formatting entirely
+              rope_autoimport = { enabled = true },
+              pylsp_mypy = { enabled = false },
+              pylsp_black = { enabled = false },
+              pyls_isort = { enabled = true },
+            },
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
